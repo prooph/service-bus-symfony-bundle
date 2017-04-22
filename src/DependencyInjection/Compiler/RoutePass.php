4a1bf4ca74dc8c1c761ec21ac6d99e53ff032984
Matthias Breddin <mb@lunetics.com>
2017-04-22 03:16:37 +0200

    Remove the message detection method, rather rely on explicit message configuration. Events can have multiple tags for each message to listen on e.g. `     My\PlaceProjector:         arguments: ['@doctrine.dbal.default_connection']         tags:             - { name: prooph_service_bus.default_event_bus.route_target, message: My\Event\FooWasCreated }             - { name: prooph_service_bus.default_event_bus.route_target, message: My\Event\FooWasUpdated } ` Query and Command bus will throw an exception if more than one message is given Existing and new events / queries will be deduplicated (avoiding multiple calls) It's better to use an InvokeStrategy as intended

--- src/DependencyInjection/Compiler/RoutePass.php
+++ src/DependencyInjection/Compiler/RoutePass.php
@@ -1,89 +1,103 @@
 <?php
 
 declare(strict_types=1);
+
 namespace Prooph\Bundle\ServiceBus\DependencyInjection\Compiler;
 
 use Prooph\Bundle\ServiceBus\DependencyInjection\ProophServiceBusExtension;
-use Prooph\Common\Messaging\HasMessageName;
-use Prooph\Common\Messaging\Message;
-use ReflectionClass;
-use ReflectionMethod;
+use Prooph\ServiceBus\Exception\RuntimeException;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class RoutePass implements CompilerPassInterface
 {
-    private static function tryToDetectMessageName(ReflectionClass $messageReflection)
-    {
-        if (! $messageReflection->implementsInterface(HasMessageName::class)) {
-            return;
-        }
-        $instance = $messageReflection->newInstanceWithoutConstructor(); /* @var $instance HasMessageName */
-        if ($messageReflection->hasMethod('init')) {
-            $init = $messageReflection->getMethod('init');
-            $init->setAccessible(true);
-            $init->invoke($instance);
-        }
-
-        return $instance->messageName();
-    }
+    /**
+     * @var array
+     */
+    private $routesFromTaggedServices = [];
 
     public function process(ContainerBuilder $container)
     {
         foreach (ProophServiceBusExtension::AVAILABLE_BUSES as $type) {
-            if (! $container->hasParameter('prooph_service_bus.' . $type . '_buses')) {
+            if (!$container->hasParameter('prooph_service_bus.'.$type.'_buses')) {
                 continue;
             }
 
-            $buses = $container->getParameter('prooph_service_bus.' . $type . '_buses');
+            $buses = $container->getParameter('prooph_service_bus.'.$type.'_buses');
 
             foreach ($buses as $name => $bus) {
+                $this->routesFromTaggedServices[$name] = [];
+                // Get existing routes from bus config
                 $router = $container->findDefinition(sprintf('prooph_service_bus.%s.router', $name));
                 $routerArguments = $router->getArguments();
 
-                $handlers = $container->findTaggedServiceIds(sprintf('prooph_service_bus.%s.route_target', $name));
+                //
+                foreach ($routerArguments[0] as $message => $routeTarget) {
+                    // If we have an eventbus, multiple route targets are possible. Only unique route targets will be added to the message
+                    if (is_array($routeTarget) && $type === 'event') {
+                        array_map(function ($routeTarget) use ($type, $message, $name) {
+                            $this->registerRoute($type, $message, $routeTarget, $name);
+                        }, array_unique($routeTarget));
+                    } else {
+                        $this->registerRoute($type, $message, $routeTarget, $name);
+                    }
 
-                foreach ($handlers as $id => $args) {
-                    foreach ($args as $eachArgs) {
-                        $messageNames = $this->recognizeMessageNames($container, $id, $eachArgs);
+                }
 
-                        if ($type === 'event') {
-                            $routerArguments[0] = array_merge_recursive(
-                                $routerArguments[0],
-                                array_combine($messageNames, array_fill(0, count($messageNames), [$id]))
-                            );
-                            $routerArguments[0] = array_map('array_unique', $routerArguments[0]);
-                        } else {
-                            $routerArguments[0] = array_merge(
-                                $routerArguments[0],
-                                array_combine($messageNames, array_fill(0, count($messageNames), $id))
-                            );
+                $routeTargetId = sprintf('prooph_service_bus.%s.route_target', $name);
+                $routeTargetServices = $container->findTaggedServiceIds($routeTargetId);
+
+                foreach ($routeTargetServices as $routeTarget => $routeTags) {
+                    // Guard: for command and query, only one tag is allowed per handler (1:1)
+                    if ($type !== 'event' && count($routeTags) > 1) {
+                        throw new RuntimeException(
+                            sprintf(
+                                'More than 1 %s handler tagged on service "%s" with tag "%s". Only events can have multiple handlers',
+                                $type, $routeTarget, $routeTargetId
+                            )
+                        );
+                    }
+
+                    foreach ($routeTags as $tag) {
+                        if (isset($tag['message']) && !empty($tag['message'])) {
+                            $this->registerRoute($type, $tag['message'], $routeTarget, $name);
+
+                            continue;
                         }
+                        // Guard: throw an early exception on misconfiguration
+                        throw new RuntimeException(sprintf('"message" tag key is missing or is empty on route target "%s"', $routeTarget));
                     }
                 }
-                $router->setArguments($routerArguments);
+                $router->setArguments([$this->getRouteArgumentsForBus($name)]);
             }
         }
     }
 
-    private function recognizeMessageNames(ContainerBuilder $container, $id, array $args)
+    private function registerRoute(string $type, string $message, $routeTarget, string $busName): void
     {
-        if (isset($args['message'])) {
-            return [$args['message']];
-        }
-        $handlerReflection = new ReflectionClass($container->getDefinition($id)->getClass());
-
-        $methodsWithMessageParameter = array_filter(
-            $handlerReflection->getMethods(ReflectionMethod::IS_PUBLIC),
-            function (ReflectionMethod $method) {
-                return $method->getNumberOfRequiredParameters() === 1
-                && $method->getParameters()[0]->getClass()
-                && $method->getParameters()[0]->getClass()->implementsInterface(Message::class);
+        if ($type !== 'event' && is_string($routeTarget)) {
+            // Disallow accidental overwriting of routes configured in the bus config with a tagged service
+            if (isset($this->routesFromTaggedServices[$message])) {
+                throw new RuntimeException(sprintf(
+                    'Route target for %s "%s" is already mapped to "%s" in the config for "%s"', $type, $message, $routeTarget, $busName
+                ));
             }
-        );
+            $this->routesFromTaggedServices[$busName][$message] = $routeTarget;
 
-        return array_filter(array_unique(array_map(function (ReflectionMethod $method) {
-            return self::tryToDetectMessageName($method->getParameters()[0]->getClass());
-        }, $methodsWithMessageParameter)));
+            return;
+        } elseif ($type === 'event' && is_string($routeTarget)) {
+            // Silently don't add existing route targets
+            $this->routesFromTaggedServices[$busName][$message][] = $routeTarget;
+            $this->routesFromTaggedServices[$busName][$message] = array_unique($this->routesFromTaggedServices[$busName][$message]);
+
+            return;
+        }
+
+        throw new RuntimeException(sprintf('Invalid config. Cannot route "%s" message "%s"', $type, $message));
+    }
+
+    private function getRouteArgumentsForBus(string $busName): array
+    {
+        return $this->routesFromTaggedServices[$busName];
     }
 }
