a9a3107c08d182581a15b19ceda58c0f115328c3
Oskar Pfeifer-Bley <oskar@programming-php.net>
2017-10-26 10:32:11 +0200

    Return exceptions from static factory instead of throwing them

--- src/Exception/CompilerPassException.php
+++ src/Exception/CompilerPassException.php
@@ -8,21 +8,21 @@ class CompilerPassException extends RuntimeException
 {
     public static function messageTagMissing(string $serviceId): self
     {
-        throw new self(
-            sprintf('The "message" tag key is missing from tag. Either provide a "message" tag or enable "message_detection" for service "%s"',
-                $serviceId
-            )
-        );
+        return new self(sprintf(
+            'The "message" tag key is missing from tag. '
+                . 'Either provide a "message" tag or enable "message_detection" for service "%s"',
+            $serviceId
+        ));
     }
 
     public static function tagCountExceeded(string $type, string $serviceId, string $busName): self
     {
-        throw new self(
-            sprintf(
-                'More than 1 %s handler tagged on service "%s" with tag "%s". Only events can have multiple handlers',
-                $type, $serviceId, $busName
-            )
-        );
+        return new self(sprintf(
+            'More than 1 %s handler tagged on service "%s" with tag "%s". Only events can have multiple handlers',
+            $type,
+            $serviceId,
+            $busName
+        ));
     }
 
     public static function unknownHandlerClass(string $className, string $serviceId, string $busName): self
